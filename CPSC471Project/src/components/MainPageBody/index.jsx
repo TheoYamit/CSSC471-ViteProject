dbec8f47189d18e840adba5d061ea076449cd5b8
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-03-17 14:22:24 -0600

    Finished navbar for now

---
+++ CPSC471Project/src/components/MainPageBody/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './mainpagebody.css'
+
+const MainPageBody = () => {
+
+    return (
+        <main className="main-page">
+            <div>
+
+            </div>
+        </main>
+        
+    );
+};
+
+export default MainPageBody
--- CPSC471Project/src/components/Navbar/index.jsx
+++ CPSC471Project/src/components/Navbar/index.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './navbar.css';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faSignIn, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSearch, faSignIn, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 const NavBar = () => {
 
     return (
-        <nav className="justify-between fixed top-0 w-full z-10 flex items-center px-4 py-2 text-white background-of-navbar">
-            <img src={logo} alt="Logo" className="h-20 w-auto" />
+        <nav className="justify-between w-full z-10 flex items-center px-4 py-2 text-white background-of-navbar">
+            <button>
+                <FontAwesomeIcon icon={faBars} style={{color: "#776B5D"}}size="2xl" className="px-4"/>
+            </button>
+            <div className="flex">
+                <img src={logo} alt="Logo" className="h-20 w-auto" />
+            </div>
+            
 
             <div className="flex-1 mx-4 flex justify-center items-center">
-                <input type="text" placeholder="I am looking for..." className="w-3/4 p-2 rounded border border-inherit h-10" style={{borderTopRightRadius: "0", borderBottomRightRadius: "0"}} />
-                <button className="px-5 h-10 border-inherit search-button" style={{borderTopLeftRadius:"0", borderBottomLeftRadius: "0"}}>
+                <input type="text" placeholder="I am looking for..." className="w-3/4 p-2 rounded border border-inherit h-10 search-bar" style={{borderTopRightRadius: "0", borderBottomRightRadius: "0"}} />
+                <button className="px-5 h-10 border-inherit search-button" style={{borderTopLeftRadius:"0", borderBottomLeftRadius: "0", borderTopRightRadius: "10px", borderBottomRightRadius: "10px"}}>
                     <FontAwesomeIcon icon={faSearch} style={{color: "#000000"}}/>
                 </button>
             </div>
 
-            <div>
+            <div className="mx-4 flex">
                 <button className="px-4 py-2 rounded transition duration-300 login-button">
                     <FontAwesomeIcon icon={faSignIn} className="px-2"/>
                     Login
@@ -28,6 +34,6 @@ const NavBar = () => {
             </div>
         </nav>
     );
-}
+};
 
 export default NavBar;
