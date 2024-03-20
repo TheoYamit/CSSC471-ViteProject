e132f7d0c14fe20fb879ca50005304a443e6a2ae
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-03-19 18:56:39 -0600

    Starting login/register pages. Added server.js.
    Can connect to database now.

---
+++ ServerBackend/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database');
+});
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error registering new user');
+    } else {
+      res.status(201).send('User registered');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
