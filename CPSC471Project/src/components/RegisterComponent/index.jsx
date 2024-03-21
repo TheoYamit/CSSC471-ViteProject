92a12e17e9a76cef1d4fe153953d1ee82eed3e38
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-03-21 14:19:21 -0600

    Adding react-hook-form to use the useForm ting

--- CPSC471Project/src/components/RegisterComponent/index.jsx
+++ CPSC471Project/src/components/RegisterComponent/index.jsx
@@ -1,11 +1,14 @@
-import react from 'react'
 import './registercomponent.css'
-import { Box, HStack, VStack, Text} from '@chakra-ui/react'
+import React, { useState } from 'react';
+import { Box, VStack, Text} from '@chakra-ui/react'
 import { useNavigate } from "react-router-dom";
-import {faUser, faEnvelope, faLocationDot} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useForm } from 'react-hook-form'
+
 
 const RegisterComponent = () => {
+
+    
+
     let navigateLogin = useNavigate();
     const routeChangeLogin = () => {
         let path = '/login'
@@ -14,63 +17,63 @@ const RegisterComponent = () => {
 
     return (
         <div className="registercomponent">
-                    <VStack>
-                        <form>
-                            <Box p="10" pt="3" pb="5" className="loginbox">
-                                <VStack>
-                                    <Text fontSize='3xl' as="b">
-                                        Register For An Account
-                                    </Text>
-                                    <input
-                                    type="text"
-                                    placeHolder="First Name"
-                                    className="username-input"
-                                    required
-                                    />
-                                    <input
-                                    type="text"
-                                    placeHolder="Last Name"
-                                    className="username-input"
-                                    required
-                                    />
+            <VStack>
+                <form>
+                    <Box p="10" pt="3" pb="5" className="loginbox">
+                        <VStack>
+                            <Text fontSize='3xl' as="b">
+                                Register For An Account
+                            </Text>
+                            <input
+                            type="text"
+                            placeHolder="First Name"
+                            className="username-input"
+                            required
+                            />
+                            <input
+                            type="text"
+                            placeHolder="Last Name"
+                            className="username-input"
+                            required
+                            />
 
-                                    <input
-                                    type="email"
-                                    placeHolder="Email Address"
-                                    className="username-input"
-                                    required
-                                    />
-                                    
-                                    <input
-                                    type="text"
-                                    placeHolder="Address"
-                                    className="username-input"
-                                    required
-                                    />
+                            <input
+                            type="email"
+                            placeHolder="Email Address"
+                            className="username-input"
+                            required
+                            />
+                            
+                            <input
+                            type="text"
+                            placeHolder="Address"
+                            className="username-input"
+                            required
+                            />
 
-                                    <input
-                                    type="text"
-                                    placeHolder="Username"
-                                    className='username-input'
-                                    required
-                                    />
-                                    <input
-                                    type="password"
-                                    placeHolder="Password"
-                                    className='username-input'
-                                    required
-                                    />
-        
-                                    <button className="login-submit">
-                                        Register
-                                    </button>
-                                </VStack>
-                            </Box>
-                        </form>
-                        <Text>Already have an account?</Text>
-                        <Text as="b" onClick={routeChangeLogin} style={{cursor: 'pointer'}}>Login now</Text>
-                    </VStack>
-                </div>
+                            <input
+                            type="text"
+                            placeHolder="Username"
+                            className='username-input'
+                            required
+                            />
+                            <input
+                            type="password"
+                            placeHolder="Password"
+                            className='username-input'
+                            required
+                            />
+
+                            <button className="login-submit">
+                                Register
+                            </button>
+                        </VStack>
+                    </Box>
+                </form>
+                <Text>Already have an account?</Text>
+                <Text as="b" onClick={routeChangeLogin} style={{cursor: 'pointer'}}>Login now</Text>
+            </VStack>
+        </div>
     );
 };
 
