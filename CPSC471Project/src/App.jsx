b3243eb02182e69935970145beb958e76f7ff985
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-03-24 09:23:59 -0600

    Starting profile. Made navbar dissapear once the window scroll was greater than 96

--- CPSC471Project/src/App.jsx
+++ CPSC471Project/src/App.jsx
@@ -3,11 +3,9 @@ import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {AuthProvider} from '../src/contexts/Authorization/Authorized'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from './fonts/fonts.jsx'
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,6 +16,7 @@ function App() {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
+                <Route path='/profile' element={<Profile/>}/>
             </Routes>
         </AuthProvider>
     </Router>
