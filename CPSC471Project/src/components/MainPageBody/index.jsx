d37769f5d0ba811efe62c07405c3538d03ed9bb6
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-03-25 15:25:47 -0600

    Added different component changes for home screen if you're an admin or a user.
    Need to add the admin control stuff after too.

--- CPSC471Project/src/components/MainPageBody/index.jsx
+++ CPSC471Project/src/components/MainPageBody/index.jsx
@@ -4,20 +4,21 @@ import saleBanner from '../../assets/sale.svg'
 import WebFont from 'webfontloader'
 
 const MainPageBody = () => {
-    WebFont.load({
-        google: {
-            families: ['Fredoka: 300, 400, 700']
-        }
-    });
-    return (
-        <main className="main-page">
-            <div className="relative flex justify-center items-center">
-                <img src={saleBanner}/>
-                
-            </div>
-        </main>
-        
-    );
+  WebFont.load({
+    google: {
+      families: ['Fredoka: 300, 400, 700']
+    }
+  });
+
+
+  return (
+    <main className="main-page">
+      <div className="relative flex justify-center items-center">
+        <img src={saleBanner} />
+      </div>
+    </main>
+
+  );
 };
 
 export default MainPageBody;
--- CPSC471Project/src/pages/Home/index.jsx
+++ CPSC471Project/src/pages/Home/index.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import NavBar from '../../components/Navbar';
 import MainPageBody from '../../components/MainPageBody';
 import MainPageBodyTwo from '../../components/MainPageBodyTwo';
+import { useAuth } from '../../contexts/Authorization/Authorized'
 
 const Home = () => {
-
-    return (
-        <>
-            <NavBar/>
-            <MainPageBody/> 
-            <MainPageBodyTwo/>
-        </>
-    );
+  const { isAuthenticated, isAdmin, userDetails } = useAuth();
+  return (
+    <>
+      {(isAuthenticated && isAdmin) ? 
+      <>
+        <NavBar />
+      </>
+      :
+      <>
+        <NavBar />
+        <MainPageBody />
+        <MainPageBodyTwo />
+      </>
+      }
+    </>
+  );
 };
 
 export default Home;
