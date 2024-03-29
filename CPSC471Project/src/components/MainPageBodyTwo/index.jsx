89980c03395c401983ed30152bd530f697d324c8
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-03-29 15:30:14 -0600

    Added fonts. Started to display products onto homsecreen.

--- CPSC471Project/src/components/MainPageBodyTwo/index.jsx
+++ CPSC471Project/src/components/MainPageBodyTwo/index.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './mainpagebodytwo.css';
-import {Flex, Box, SimpleGrid, Stack, HStack, VStack, Badge, Text } from '@chakra-ui/react'
+import { Flex, Box, SimpleGrid, Stack, HStack, VStack, Badge, Text } from '@chakra-ui/react'
 import ProductBox from '../ProductBox';
 import ProductBoxNew from '../ProductBoxNew'
 import ProductBoxDiscounted from '../ProductBoxDiscounted';
@@ -8,21 +8,62 @@ import ProductBoxNewDiscounted from '../ProductBoxNewDiscounted';
 
 const MainPageBodyTwo = () => {
 
-  const [productDetails, setProductDetails] = useState();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProductsCategory = async (data) => {
+      const response = await fetch('http://localhost:3001/getproductclothing', {
+        method: "GET"
+      });
+      const responseFromServer = await response.json();
+      setProducts(responseFromServer.products);
+    };
+    fetchProductsCategory();
+  }, []);
+
   return (
     <>
-      <Flex 
-      className="mainpagebodytwo"
-      direction="row" 
-      overflowX="scroll" 
-      sx={{
+      <Text px={5} py={5} fontSize="5xl" fontFamily="adineue PRO Bold" bg="#F3EEEA">Our Popular Clothing</Text>
+      <Flex
+        className="mainpagebodytwo"
+        direction="row"
+        overflowX="scroll"
+        sx={{
           '&::-webkit-scrollbar': {
             display: 'none',
           },
-          '-ms-overflow-style': 'none', 
+          '-ms-overflow-style': 'none',
           'scrollbar-width': 'none',
         }}
-      scrollBehaviour="smooth">
+        scrollBehaviour="smooth">
+
+        {console.log(products)}
+        {products.map((product) => {
+          const Component = product.IsNew == 1
+            ? product.IsDiscounted == 1 ? ProductBoxNewDiscounted : ProductBoxNew
+            : product.IsDiscounted == 1 ? ProductBoxDiscounted : ProductBox;
+
+
+          const arrayBufferView = new Uint8Array(product.Image.data);
+          const blob = new Blob([arrayBufferView], { type: "image/png" });
+          const imageUrl = URL.createObjectURL(blob);
+          return (
+            <Component px={5}
+              key={product.ProductID}
+              productID={product.ProductID}
+              nameOfProduct={product.Name}
+              descOfProduct={product.Description}
+              priceOfProduct={product.Price}
+              imageOfProduct={imageUrl}
+              categoryOfProduct={product.Category}
+              genderOfProduct={product.Gender}
+              isNew={product.IsNew}
+              isDiscounted={product.IsDiscounted}
+              previousPrice={product.Price}
+              discountedPrice={product.DiscountedPrice}
+            />
+          );
+        })}
 
       </Flex>
     </>
