5b4aaf62bbd87a8a1a17814f4df77934d378185a
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2024-04-04 16:38:37 -0600

    Added and setup a customer orders page for their view

---
+++ CPSC471Project/src/pages/CustomerOrdersPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../../components/Navbar';
+import CustomerOrders from '../../components/CustomerOrders';
+
+const CustomerOrdersPage = () => {
+
+  return (
+    <>
+      <NavBar/>
+      <CustomerOrders/>
+    </>
+  );
+};
+
+export default CustomerOrdersPage;
